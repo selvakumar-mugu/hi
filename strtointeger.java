fcd1371dcd6de2b01acf55309e38b2cd56f41c27
selvakumar-mugu <nskselva22@gmail.com>
2016-08-12 15:24:22 +0530

    Add files via upload

---
+++ strtointeger.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import sun.applet.Main;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class strtointeger {
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String s= in.next();
+        int i=Integer.parseInt(s);
+        System.out.println(i);
+        
+
+    }
+    
+}
