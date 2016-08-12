64d523d0f7fa461b2f17d80e76428a32287cf304
selvakumar-mugu <nskselva22@gmail.com>
2016-08-12 09:37:15 +0530

    Add files via upload

---
+++ NewClass2.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class NewClass2 {
+    public static void main(String[] args) {
+        
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int f=1;
+        for(int i=2;i<=a;i++){
+             f=f*i;
+            
+            
+            
+        }
+            System.out.println(f);
+        
+    }
+    
+}
