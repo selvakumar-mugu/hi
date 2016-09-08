4ca7405b3b746b4ed1745dd9de4b25d854d19a91
selvakumar-mugu <nskselva22@gmail.com>
2016-09-08 17:50:57 +0530

    Add files via upload

--- NewClass.java
+++ NewClass.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 
 
 public class NewClass {
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
-        String a = "";
+        String sz=s.next();
         
-        System.out.println("enter the length of string");
-       int n =s.nextInt();
-       String []sarr=new String[n];
-        System.out.println("enter the rotae element");
-       int k=s.nextInt();
-       System.out.println("enter the  elements");
-        for(int i=0;i<n;i++){
-            sarr[i]=s.next();
-            a+=sarr[i];
-           }
-        String ans=(a.substring(n-k)+a.substring(0, n-k)).replace("", ",");
-        int l=ans.length();
-        System.out.println(ans.substring(1, l-1));
+        String b;
+        StringBuffer a=new StringBuffer();
+        b=sz.replaceAll("[aeiou]","");
+        System.out.println(b);
+         StringBuffer df=new StringBuffer(b);
+        a=df.reverse();
+        
+        System.out.println(a);         ;
+        
+       
+      
+        
+        
+        
+    }
     
-}
 }
